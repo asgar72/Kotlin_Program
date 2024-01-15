26d17833e1bea73fc537f6f4468c99778a2f1b79
asgar72 <72asgar72@gmail.com>
2024-01-15 11:05:24 +0530

    Bank Account program added

--- src/main/kotlin/bankAccountProgram/BankMain.kt
+++ src/main/kotlin/bankAccountProgram/BankMain.kt
@@ -1,5 +1,14 @@
 package bankAccountProgram
 
 fun main(){
-        
+        val abbasBankAccount = BankAccount("Asgar Abbas",1478.20)
+        abbasBankAccount.deposit(400.0)
+        abbasBankAccount.withdraw(1500.00)
+        abbasBankAccount.deposit(3000.00)
+        abbasBankAccount.deposit(2000.00)
+        abbasBankAccount.withdraw(3232.20)
+
+        abbasBankAccount.displayTransactionHistory()
+    println("${abbasBankAccount.accountHolder}'s " +
+            "balance is $${abbasBankAccount.balance}")
 }
